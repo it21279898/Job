775baf4a1c3fa717ca0db0b5829b79a45f7ecc72
Kula <99582220+it21279898@users.noreply.github.com>
2023-04-16 10:11:18 +0530

    main parts are done

--- app/src/main/java/com/example/job/HomeActivity.kt
+++ app/src/main/java/com/example/job/HomeActivity.kt
@@ -1,11 +1,22 @@
 package com.example.job
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.job.databinding.ActivityHomeBinding
+
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogout.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+
+        }
     }
 }
