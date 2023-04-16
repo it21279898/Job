1caee1951896397cd905e71aedc6247670f96c48
Kula <99582220+it21279898@users.noreply.github.com>
2023-04-16 12:33:02 +0530

    add validation for register form and add the firebase dependencies

--- app/src/main/java/com/example/job/LoginActivity.kt
+++ app/src/main/java/com/example/job/LoginActivity.kt
@@ -1,15 +1,14 @@
 package com.example.job
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.example.job.databinding.ActivityLoginBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
 
-
-
-
-
-
+@SuppressLint("CheckResult")
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
@@ -17,6 +16,50 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //username validation
+        val usernameSteam = RxTextView.textChanges(binding.etEmail)
+            .skipInitialValue()
+            .map { username->
+                username.isEmpty()
+            }
+        usernameSteam.subscribe{
+            showTextMinimalAlert(it,"Email/Username")
+        }
+
+        //password validation
+        val passwordSteam = RxTextView.textChanges(binding.etPassword)
+            .skipInitialValue()
+            .map { password->
+                password.isEmpty()
+            }
+        passwordSteam.subscribe{
+            showTextMinimalAlert(it,"Password")
+        }
+
+        //button enable true or false
+        val invalidFieldsStream = io.reactivex.Observable.combineLatest(
+
+            usernameSteam,
+            passwordSteam,
+            {usernameInvalid :Boolean,passwordInvalid :Boolean->
+               !usernameInvalid && !passwordInvalid
+            })
+
+        invalidFieldsStream.subscribe { isValid->
+            if(isValid){
+                binding.btnLogin.isEnabled=true
+                binding.btnLogin.backgroundTintList= ContextCompat.getColorStateList(this,R.color.primary_color)
+            } else {
+                binding.btnLogin.isEnabled=false
+                binding.btnLogin.backgroundTintList= ContextCompat.getColorStateList(this,android.R.color.darker_gray)
+
+            }
+        }
+
+
+
+        //click
 
         binding.btnLogin.setOnClickListener{
             startActivity(Intent(this, HomeActivity::class.java))
@@ -26,10 +69,10 @@ class LoginActivity : AppCompatActivity() {
         }
     }
     private fun showTextMinimalAlert(isNotValid:Boolean,text:String) {
-        if (text == "Username")
-            binding.etEmail.error = if (isNotValid) "$text tidak boleh kosong!" else null
+        if (text == "Email/Username")
+            binding.etEmail.error = if (isNotValid) "$text Cannot be empty!" else null
         else if (text == "Password")
-            binding.etPassword.error = if (isNotValid) "$text tidak boleh kosong!" else null
+            binding.etPassword.error = if (isNotValid) "$text This cannot be empty!" else null
     }
 
 }
--- app/src/main/java/com/example/job/RegisterActivity.kt
+++ app/src/main/java/com/example/job/RegisterActivity.kt
@@ -1,13 +1,16 @@
 package com.example.job
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.database.Observable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import androidx.core.content.ContextCompat
 import com.example.job.databinding.ActivityRegisterBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
 
-
-
-
+@SuppressLint("CheckResult")
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
 
@@ -16,7 +19,88 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+// fullname validation
+        val nameStream = RxTextView.textChanges(binding.etFullname)
+            .skipInitialValue()
+            .map { name->
+                name.isEmpty()
+            }
+        nameStream.subscribe{
+            showNameExistAlert(it)
+        }
+        //Email Validation
 
+        val emailStream =RxTextView.textChanges(binding.etEmail)
+            .skipInitialValue()
+            .map { email->
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches()
+            }
+        emailStream.subscribe{
+            showEmailValidtAlert(it)
+        }
+
+        //username validation
+        val usernameSteam = RxTextView.textChanges(binding.etUsername)
+            .skipInitialValue()
+            .map { username->
+                username.length<6
+            }
+        usernameSteam.subscribe{
+            showTextMinimalAlert(it,"Username")
+        }
+
+        //password validation
+        val passwordSteam = RxTextView.textChanges(binding.etPassword)
+            .skipInitialValue()
+            .map { password->
+                password.length<6
+            }
+        passwordSteam.subscribe{
+            showTextMinimalAlert(it,"Password")
+        }
+
+        //confirm password validation
+        val passwordConfirmStream = io.reactivex.Observable.merge(
+            RxTextView.textChanges(binding.etPassword)
+                .skipInitialValue()
+                .map { password->
+                    password.toString() != binding.etComfirmPassword.text.toString()
+                },
+            RxTextView.textChanges(binding.etComfirmPassword)
+                .skipInitialValue()
+                .map { confirmPassword->
+                    confirmPassword.toString() !=binding.etPassword.text.toString()
+                })
+        passwordConfirmStream.subscribe{
+            showPasswordComfirmAlert(it)
+        }
+
+        //button enable true or false
+         val invalidFieldsStream = io.reactivex.Observable.combineLatest(
+             nameStream,
+             emailStream,
+             usernameSteam,
+             passwordSteam,
+             passwordConfirmStream,
+             {nameInvalid :Boolean ,emailInvalid :Boolean ,usernameInvalid :Boolean,passwordInvalid :Boolean,passwordConfirmInvalid :Boolean ->
+                 !nameInvalid && !emailInvalid && !usernameInvalid && !passwordInvalid && !passwordConfirmInvalid
+             })
+
+        invalidFieldsStream.subscribe { isValid->
+            if(isValid){
+                binding.btnRegister.isEnabled=true
+                binding.btnRegister.backgroundTintList= ContextCompat.getColorStateList(this,R.color.primary_color)
+            } else {
+                binding.btnRegister.isEnabled=false
+                binding.btnRegister.backgroundTintList= ContextCompat.getColorStateList(this,android.R.color.darker_gray)
+
+            }
+        }
+
+
+
+
+//click
 
 
         binding.btnRegister.setOnClickListener{
@@ -28,18 +112,18 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun showNameExistAlert(isNotValid:Boolean){
-        binding.etFullname.error = if(isNotValid)"Nama tidak boleh kosong!" else null
+        binding.etFullname.error = if(isNotValid)"Name cannot be empty!" else null
     }
     private fun showTextMinimalAlert(isNotValid:Boolean,text:String) {
         if (text == "Username")
-            binding.etUsername.error = if (isNotValid) "$text harus lebih dari 6 huruf!" else null
+            binding.etUsername.error = if (isNotValid) "$text The Username must be more than 6 characters!" else null
         else if (text == "Password")
-            binding.etPassword.error = if (isNotValid) "$text harus lebih dari 6 huruf!" else null
+            binding.etPassword.error = if (isNotValid) "$text The password must be at least 6 characters long!" else null
     }
     private fun showEmailValidtAlert(isNotValid:Boolean){
-        binding.etEmail.error = if(isNotValid)"Email tidak valid!" else null
+        binding.etEmail.error = if(isNotValid)"Email is not valid!" else null
     }
     private fun showPasswordComfirmAlert(isNotValid:Boolean){
-        binding.etComfirmPassword.error = if(isNotValid)"Password tidak sama!" else null
+        binding.etComfirmPassword.error = if(isNotValid)"Passwords do not match!" else null
     }
 }
