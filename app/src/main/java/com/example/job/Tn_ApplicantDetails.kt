25b3229b903184f77f2867f95746b5bf535bd8d2
IT21219702 <it21219702@my.sliit.lk>
2023-05-06 19:25:43 +0530

    updated

--- app/src/main/java/com/example/job/Tn_ApplicantDetails.kt
+++ app/src/main/java/com/example/job/Tn_ApplicantDetails.kt
@@ -3,7 +3,11 @@ package com.example.job
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.database.FirebaseDatabase
 
 class Tn_ApplicantDetails : AppCompatActivity() {
 
@@ -22,8 +26,14 @@ class Tn_ApplicantDetails : AppCompatActivity() {
         setContentView(R.layout.activity_tn_applicant_details)
 
         initView()
-
         setValuesToViews()
+
+        btnUpdate.setOnClickListener {
+            openUpdateDialog(
+                intent.getStringExtra("applicantId").toString(),
+                intent.getStringExtra("name").toString()
+            )
+        }
     }
 
     private fun initView(){
@@ -39,7 +49,6 @@ class Tn_ApplicantDetails : AppCompatActivity() {
         btnDelete = findViewById(R.id.btnDelete)
 
     }
-
     private  fun setValuesToViews(){
         tvApplicantId.text = intent.getStringExtra("applicantId")
         tvName.text = intent.getStringExtra("name")
@@ -55,4 +64,77 @@ class Tn_ApplicantDetails : AppCompatActivity() {
 
 
     }
+    private fun openUpdateDialog(
+        applicantId : String,
+        ApplicantName : String
+    ){
+        val mDialog = AlertDialog.Builder(this)
+        val inflater = layoutInflater
+        val mDialogView = inflater.inflate(R.layout.tn_update_applicant_details, null)
+
+        mDialog.setView(mDialogView)
+
+        val appName = mDialogView.findViewById<EditText>(R.id.etName)
+        val appEmail = mDialogView.findViewById<EditText>(R.id.etEmail)
+        val appNumber = mDialogView.findViewById<EditText>(R.id.etNumber)
+        val appJob = mDialogView.findViewById<EditText>(R.id.etJobName)
+        val appPosition = mDialogView.findViewById<EditText>(R.id.etPosition)
+        val appStatus = mDialogView.findViewById<EditText>(R.id.etStatus)
+
+        val updateBtn = mDialogView.findViewById<Button>(R.id.btnUpdateData)
+
+        appName.setText(intent.getStringExtra("name").toString())
+        appEmail.setText(intent.getStringExtra("email").toString())
+        appNumber.setText(intent.getStringExtra("phoneNo").toString())
+        appJob.setText(intent.getStringExtra("jobName").toString())
+        appPosition.setText(intent.getStringExtra("position").toString())
+        appStatus.setText(intent.getStringExtra("status").toString())
+
+        mDialog.setTitle("Updating $appName Record")
+
+        val alertDialog = mDialog.create()
+        alertDialog.show()
+
+        btnUpdate.setOnClickListener {
+            updateApplicantData(
+                applicantId,
+                appName.text.toString(),
+                appEmail.text.toString(),
+                appNumber.text.toString(),
+                appJob.text.toString(),
+                appPosition.text.toString(),
+                appStatus.text.toString()
+
+                )
+            Toast.makeText(applicationContext,"Applicant Data Updated", Toast.LENGTH_LONG).show()
+
+            //setting updated data to our textView
+            tvName.text = appName.text.toString()
+            tvEmail.text = appEmail.text.toString()
+            tvNumber.text = appEmail.text.toString()
+            tvJobName.text = appNumber.text.toString()
+            tvPosition.text = appPosition.text.toString()
+            tvStatus.text = appStatus.text.toString()
+
+            alertDialog.dismiss()
+        }
+
+
+    }
+
+    private fun updateApplicantData(
+        id:String,
+        name:String,
+        email:String,
+        phone:String,
+        job:String,
+        position:String,
+        status:String
+    ){
+        val dbRef = FirebaseDatabase.getInstance().getReference("Applicant").child(id)
+        val applicantInfo = Tn_ApplicantModel(id,name,email,phone,job,position,status)
+
+        dbRef.setValue(applicantInfo)
+    }
+
 }
