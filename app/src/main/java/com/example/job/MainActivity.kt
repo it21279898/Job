71614f88162f8ff1e4db0685eb5fac38e55c23fc
IT21219702 <it21219702@my.sliit.lk>
2023-05-06 12:32:05 +0530

    CRUD

--- app/src/main/java/com/example/job/MainActivity.kt
+++ app/src/main/java/com/example/job/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.job
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import com.example.job.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -10,6 +11,10 @@ import com.google.firebase.auth.FirebaseAuth
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+
+//    Tharanee
+    private lateinit var btnApply: Button
+    private lateinit var btnView: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +29,19 @@ class MainActivity : AppCompatActivity() {
         binding.btnRegister.setOnClickListener{
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+
+        btnApply = findViewById(R.id.btn_Apply)
+        btnView = findViewById(R.id.btn_View)
+
+        btnApply.setOnClickListener {
+            val intent = Intent(this,Tn_jobApplication::class.java)
+            startActivity(intent)
+        }
+        btnView.setOnClickListener {
+            val intent = Intent(this, Tn_fetchData::class.java)
+            startActivity(intent)
+        }
+
 
     }
 
